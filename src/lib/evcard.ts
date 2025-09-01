3f6452021e9bd18338d7a289183084ac969e869b
HuMiao <humiao2006@gmail.com>
2025-09-01 15:14:31 +0800

    签到成功3333333

--- src/lib/evcard.ts
+++ src/lib/evcard.ts
@@ -102,7 +102,7 @@ export async function makeSignInRequest(env: Env, token: string, accountName: st
 
         const result: SignInResult = {
             success: responseData.code === 200,
-            message: '签到成功',
+            message: '签到成功：' + responseData.data,
             data: responseData.data
         };
 
